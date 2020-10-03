6fe5a6544fd257ea373a7181231cd2b0740b8ce8
sdaskaliesku <manson.serg@gmail.com>
2020-10-03 15:54:27 +0300

    Add text value for leg. mod

--- src/main/kotlin/com/manson/domain/LegendaryMod.kt
+++ src/main/kotlin/com/manson/domain/LegendaryMod.kt
@@ -5,35 +5,45 @@ import org.apache.commons.lang3.builder.EqualsBuilder
 import org.apache.commons.lang3.builder.HashCodeBuilder
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class LegendaryMod @JvmOverloads constructor(var value: String? = null, var star: Int = 999, var abbreviation: String? = null) {
-    var id: String? = null
-    var gameId: String? = null
+class LegendaryMod @JvmOverloads constructor(
+    var value: String = "",
+    var star: Int = 999,
+    var abbreviation: String = ""
+) {
+    var id: String = ""
+    var gameId: String = ""
+    var text: String = ""
 
-    override fun equals(o: Any?): Boolean {
-        if (this === o) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
             return true
         }
-        if (o !is LegendaryMod) {
+        if (other !is LegendaryMod) {
             return false
         }
         return EqualsBuilder()
-                .append(star, o.star)
-                .append(value, o.value)
-                .append(abbreviation, o.abbreviation)
-                .append(id, o.id)
+                .append(star, other.star)
+                .append(value, other.value)
+                .append(text, other.text)
+                .append(abbreviation, other.abbreviation)
+                .append(id, other.id)
                 .isEquals
     }
 
     override fun hashCode(): Int {
         return HashCodeBuilder(17, 37)
-                .append(value)
-                .append(star)
-                .append(abbreviation)
-                .append(id)
-                .toHashCode()
+            .append(value)
+            .append(text)
+            .append(star)
+            .append(abbreviation)
+            .append(id)
+            .toHashCode()
     }
 
     override fun toString(): String {
-        return "LegendaryMod(value=$value, star=$star, abbreviation=$abbreviation, id=$id, gameId=$gameId)"
+        return "LegendaryMod(value=$value, star=$star, abbreviation=$abbreviation, id=$id, gameId=$gameId, text=$text)"
     }
+
+
 }
